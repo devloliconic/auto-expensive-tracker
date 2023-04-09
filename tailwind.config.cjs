221b8459e4a36565c377a15a977a2c122cc1642a
kirill smirnov <smirnovke@aldera-soft.ru>
2023-04-09 16:03:39 +0700

    feat: add colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontSize: {
-        DEFAULT: "1rem",
-        "2lg": "2rem"
+        DEFAULT: '1rem',
+        '2lg': '2rem',
+      },
+      colors: {
+        gray: '#717171',
+        darkGray: '#c6c6c6',
+        redBorder: '#ff7D7D',
+        redFill: '#FF9E9E',
       },
     },
-    container: {
-
-    }
+    container: {},
   },
   plugins: [],
-}
+};
