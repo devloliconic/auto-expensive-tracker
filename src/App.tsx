3a5a80c952248490910200bf9e4ba507a4b2e401
Kirill Smirnov <81454826+devloliconic@users.noreply.github.com>
2023-04-09 16:21:49 +0700

    Revert "feat: I have added an authorization form"
    This reverts commit 3aea53cd0d18275c9fca2482758d002bbfec45b0.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
-import { AuthPage } from './pages/AuthPage';
+interface FormValues {
+  vim: string;
+}
 
 export const App = () => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isValid },
+  } = useForm<FormValues>({ mode: 'onChange' });
+
+  const onSubmit = (data: FormValues) => {
+    console.log(data);
+  };
+
   return (
-    <div>
-      <AuthPage />
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex align-center justify-center p-10">
+      <input
+        className="outline-none px-3 py-1 text-gray-800 mr-2 border-2"
+        {...register('vim', { maxLength: 16, minLength: 16 })}
+      />
+
+      <button className="px-3 py-1 bg-green-500 border-2" disabled={!isValid}>
+        Войти
+      </button>
+      <button className="px-3 py-1 bg-green-500 border-2 " type="reset" onClick={() => reset()}>
+        Сбросить
+      </button>
+    </form>
   );
 };
