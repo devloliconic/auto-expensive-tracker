2901af44c510360fd029e2c9d0758eccda0adb25
kirill smirnov <smirnovke@aldera-soft.ru>
2023-04-09 16:10:48 +0700

    ref: added styling for the entrance form

--- src/pages/AuthPage/AuthPage.tsx
+++ src/pages/AuthPage/AuthPage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 
+import { Button } from '@/components/Button';
+
 import styles from './AuthPage.module.scss';
 
 interface FormValues {
   vim: string;
 }
 
-export const AuthPage: React.FC = () => {
+const AuthPage: React.FC = () => {
   const {
     register,
     handleSubmit,
@@ -22,12 +24,14 @@ export const AuthPage: React.FC = () => {
     <div className={styles.pageContainer}>
       <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
         <h1 className={styles.welcomeTitle}>Добро пожаловать!</h1>
-        <input {...register('vim', { maxLength: 17, minLength: 17 })} />
-        <h3>Введите VIN Вашего авто для авторизации</h3>
-        <button className=" px-3 py-1 bg-green-500 border-2" disabled={!isValid}>
-          Войти
-        </button>
+        <input {...register('vim', { maxLength: 17, minLength: 17 })} placeholder="VIN Вашего авто" />
+        <label>Введите VIN Вашего авто для авторизации</label>
+        <Button variant="box" type="button" size={'small'}>
+          Отслеживать
+        </Button>
       </form>
     </div>
   );
 };
+
+export default AuthPage;
