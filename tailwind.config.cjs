c636c11644769dc6f2b7e08bc3e1d0aa3c984ee9
Kirill Smirnov <81454826+devloliconic@users.noreply.github.com>
2023-04-09 16:21:49 +0700

    Revert "feat: add colors to tailwind config"
    This reverts commit 6d7d8c46fbd9efcc78be43f2b1cb440dce9f4e38.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       fontSize: {
-        DEFAULT: '1rem',
-        '2lg': '2rem',
-      },
-      colors: {
-        gray: '#717171',
-        darkGray: '#c6c6c6',
-        redBorder: '#ff7D7D',
-        redFill: '#FF9E9E',
+        DEFAULT: "1rem",
+        "2lg": "2rem"
       },
     },
-    container: {},
+    container: {
+
+    }
   },
   plugins: [],
-};
+}
