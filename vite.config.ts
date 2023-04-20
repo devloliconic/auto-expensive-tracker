3103b0fb48acfa35d40afeb22dff356449022921
kirill smirnov <smirnovke@aldera-soft.ru>
2023-04-21 01:37:24 +0700

    feat: add vite svgr dependency

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import eslintPlugin from 'vite-plugin-eslint';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin({ cache: false })],
+  plugins: [react(), eslintPlugin({ cache: false }), svgr()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
