b1c9a363a75e979482dd958cb6b8ece8ae7e1ab2
kirill smirnov <smirnovke@aldera-soft.ru>
2023-05-02 22:44:42 +0700

    feat: sync profile page with firebase

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,36 +1,57 @@
-import React from 'react';
+import { DocumentData } from 'firebase/firestore';
+import React, { useEffect, useMemo, useState } from 'react';
 
+import { useGetUserByVin } from '@/api/queries/useGetUserByVin';
 import { Button } from '@/components/Button';
 import Layout from '@/components/Layout';
 
 import styles from './ProfilePage.module.scss';
 
 const ProfilePage = () => {
+  const { getUserByVin } = useGetUserByVin();
+
+  const [userData, setUserData] = useState<DocumentData | undefined>(undefined);
+  console.log(userData);
+  const userName = useMemo(
+    () => [userData?.lastName, userData?.firstName, userData?.userYears].filter(Boolean).join(' '),
+    [userData?.firstName, userData?.lastName, userData?.userYears],
+  );
+
+  const userVin = localStorage.getItem('USER_VIN');
+  useEffect(() => {
+    if (!userData) {
+      getUserByVin(userVin || '').then((data) => {
+        setUserData(data?.docs[0]?.data());
+      });
+    }
+  }, [getUserByVin, userData, userVin]);
+
   return (
     <Layout>
       <div className={styles.pageWrapper}>
         <h2>Профиль</h2>
         <div className={styles.userInfoContainer}>
-          <p>Виктор, 31 год</p>
-          <p>Я владею 1 автомобилем!</p>
-          <Button variant="rounded" size="small">
+          {userName && <p>{userName}</p>}
+          <Button variant="box" size="small">
             Редактировать профиль
           </Button>
         </div>
-        <div className={styles.carInfoContainer}>
-          <div>
-            <img
-              src="http://st.gde-fon.com/wallpapers_original/83603_aston-martin_gauntlet_1920x1200_www.Gde-Fon.com.jpg"
-              alt="car"
-            />
-            <p>lada vesta</p>
+        {userData?.nameAuto && (
+          <div className={styles.carInfoContainer}>
+            <div>
+              <img
+                src="http://st.gde-fon.com/wallpapers_original/83603_aston-martin_gauntlet_1920x1200_www.Gde-Fon.com.jpg"
+                alt="car"
+              />
+              <p>userData?.nameAuto</p>
+            </div>
+            <div>
+              {userData?.carRange && <p>Пробег: {userData?.carRange} км</p>}
+              {userData?.years && <p>Год: {userData?.years}</p>}
+              {userData?.vin && <p>VIN: {userData?.vin}</p>}
+            </div>
           </div>
-          <div>
-            <p>Пробег: 402394 км</p>
-            <p>Год: 2017</p>
-            <p>VIN: XTAGFLHY0482343</p>
-          </div>
-        </div>
+        )}
       </div>
     </Layout>
   );
